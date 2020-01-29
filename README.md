60e6f837e8a96ae6534720c795e8c865518074c4
TE-Good <tom.good1990@gmail.com>
2020-01-29 20:28:22 +0000

    adding comments and quick clean up

---
+++ README.md
@@ -0,0 +1,16 @@
+## Hullabalook Code Challenge
+# General Assembly Software Engineering : Whatify
+
+# Timeframe
+Approx. 5 hours
+
+# Technologies Used
+* Vuejs
+* JavaScript (ES6)
+* HTML5
+* CSS3
+* Git
+
+# Installation
+
+Ensure Vue.js is installed & to open the project within your directory in your console excute `open index.html`.
--- js/app.js
+++ js/app.js
@@ -5,52 +5,42 @@ var app = new Vue({
     return {
       gameScreen: 0,
       gameCounter: 0,
-      isActive: true,
-      hasWon: false,
-      seconds: 0.00,
-      tilesClicked: [],
       optionProduct: null,
       optionBrand: null,
+      // LEFT-SIDE PRODUCTS
       products: [
-        { 
-          name: 'A', 
-          image: 'https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto,fl_lossy/6a48858b62374aed8449a8af011d27ba_9366/Lite_Racer_CLN_Shoes_Blue_B96566_01_standard.jpg',
-          brand: 'adidas' 
-        }, 
         {
-          name: 'B', 
+          image: 'https://assets.adidas.com/images/w_840,h_840,f_auto,q_auto,fl_lossy/6a48858b62374aed8449a8af011d27ba_9366/Lite_Racer_CLN_Shoes_Blue_B96566_01_standard.jpg',
+          brand: 'adidas'
+        },
+        {
           image: 'https://assets.reebok.com/images/w_840,h_840,f_auto,q_auto:sensitive,fl_lossy/c55281ac20fe4d92bc6eaa54015ab26a_9366/Nano_9.0_Shoes_Blue_DV6340_01_standard.jpg',
           brand: 'reebok'
-        }, 
+        },
         {
-          name: 'C', 
           image: 'https://i1.adis.ws/i/jpl/jd_214003_a?qlt=80&w=600&h=425&v=1&fmt=webp',
           brand: 'nike'
-        }, 
+        },
         {
-          name: 'D', 
           image: 'https://nb.scene7.com/is/image/NB/mcruznn2_nb_02_i?$pdpPictLG2x$&fmt=webp',
           brand: 'new balance'
         }
       ],
+      // RIGHT-SIDE BRANDS
       brands: [
-        { 
-          name: 'A', 
-          image: 'http://clipart-library.com/images_k/adidas-logo-transparent-background/adidas-logo-transparent-background-18.png',
-          brand: 'adidas' 
-        }, 
         {
-          name: 'B', 
+          image: 'http://clipart-library.com/images_k/adidas-logo-transparent-background/adidas-logo-transparent-background-18.png',
+          brand: 'adidas'
+        },
+        {
           image: 'https://i.imgur.com/uEfZwmy.png',
           brand: 'reebok'
-        }, 
+        },
         {
-          name: 'C', 
           image: 'https://www.pinclipart.com/picdir/big/378-3789155_view-more-nike-logo-transparent-background-clipart.png',
           brand: 'nike'
-        }, 
+        },
         {
-          name: 'D', 
           image: 'https://i.imgur.com/dfjC8yV.png',
           brand: 'new balance'
         }
@@ -59,24 +49,11 @@ var app = new Vue({
   },
 
   methods: {
-    start() {
-      this.isActive = true;
-      this.startTimer()
-    },
-    startTimer() {
-      let timer = setInterval(() => {
-        this.seconds += 0.10;
-      }, 100)
-    },
-    handleTileClick(tile) {
-      this.tilesClicked.push(tile)
-    },
-    stop() {
-      this.isActive = false
-    },
+    // RANDOMISER FOR GAME TILES
     randomise() {
       return 0.5 - Math.random()
     },
+    // HANDLING PRODUCT SELECTION
     productSelect(item, event) {
       this.removeSideHighlights(event.target)
       event.target.classList.add('highlighted')
@@ -84,6 +61,7 @@ var app = new Vue({
       this.optionProduct = { brand: item.brand, target: event.target }
       this.compareSelected()
     },
+    // HANDLING BRAND SELECTION
     brandSelect(item, event) {
       this.removeSideHighlights(event.target)
       event.target.classList.add('highlighted')
@@ -91,17 +69,21 @@ var app = new Vue({
       this.optionBrand = { brand: item.brand, target: event.target }
       this.compareSelected()
     },
+    // COMPAIRING SELECTIONS
     compareSelected() {
       if (this.optionProduct && this.optionBrand) {
         if (this.optionProduct.brand === this.optionBrand.brand) {
           this.optionProduct.target.classList.add('fadeOut')
           this.optionBrand.target.classList.add('fadeOut')
+          // ADDING TO PLAYER SCORE
           this.gameCounter++
+          // GAME WIN CONDITION CHECK
           this.gameCounter === 4 ? this.changeToEndScreen(this.optionProduct.target) : null
           this.optionProduct = null
           this.optionBrand = null
-          
+
         } else {
+          // ERROR HIGHLIGHTING & RESET
           this.removeSideHighlights(this.optionProduct.target)
           this.removeSideHighlights(this.optionBrand.target)
           this.optionProduct.target.classList.add('wrong')
@@ -111,19 +93,21 @@ var app = new Vue({
             this.optionBrand.target.classList.remove('wrong')
             this.optionProduct = null
             this.optionBrand = null
-
           }, 1000)
         }
-      } 
+      }
     },
-    removeSideHighlights(target){
+    // REMOVING SELECTION HIGHLIGHTS ACROSS ALL TILES
+    removeSideHighlights(target) {
       target.parentNode.childNodes.forEach(elem => elem.classList.remove('highlighted'))
     },
+    // ANIMATION FROM START SCREEN TO GAME SCREEN
     changeToGameScreen(event) {
       event.target.parentNode.classList.remove('fadeInDown')
       event.target.parentNode.classList.add('fadeOutDown')
       setTimeout(() => this.gameScreen++, 500)
     },
+    // ANIMATION FROM GAME SCREEN TO END SCREEN
     changeToEndScreen(target) {
       target.parentNode.parentNode.parentNode.parentNode.classList.remove('fadeInUp')
       target.parentNode.parentNode.parentNode.parentNode.classList.add('fadeOutRight')
